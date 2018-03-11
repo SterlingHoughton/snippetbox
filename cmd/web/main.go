3bd36593bd8913d2c4b1d998e97d45a304c6cd6f
sterling <sterlinghoughton@gmail.com>
2018-03-11 13:48:29 -0700

    remove tmp file create from Fresh (look into getting this working) add command line flags for http addr and static file path

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define command-line flags for network address and location of static files
+	// directory.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
+
+	// Parse command line flags.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet", ShowSnippet)
@@ -13,16 +22,16 @@ func main() {
 
 	// Create a file server which serves files out of the "./ui/static" directory.
 	// Path given to http.Dir function is relative to the project repository root.
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// Derefrence pointer passed from flag.String().
+	fileServer := http.FileServer(http.Dir(*staticDir))
 
 	// Use the mux.Handle() function to register the file server as the
 	// handler for all URL paths that start with "/static/". For matching
-	// paths, strip the "/static" prefix before the request reaches the 
+	// paths, strip the "/static" prefix before the request reaches the
 	// file server.
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
